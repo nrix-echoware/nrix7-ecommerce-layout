08788c8ac182be6853a56380289bde0d1d346295
Sourav Ahmed <souravsunju@gmail.com>
2025-10-08 21:52:15 +0530

    fixes for https

--- backend/main.go
+++ backend/main.go
@@ -89,5 +89,7 @@ func main() {
 
 	// Start server
 	logrus.Info("Server running on :9997")
-	r.Run(":9997")
+	if err := r.RunTLS(":9997", "cert/cert.pem", "cert/key.pem"); err != nil {
+		logrus.Fatal(err)
+	}
 }
